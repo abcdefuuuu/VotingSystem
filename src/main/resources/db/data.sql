b0db57a978a3db33dcc89b161b7a32e53e1731c9
zowie.lin <zowie.lin@vpon.com>
2024-08-15 18:27:13 +0800

    add vote recode

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,1 +1,13 @@
-INSERT INTO voting_items (name) VALUES ('電腦'), ('滑鼠');
+INSERT INTO voting_item (name) VALUES
+    ('電腦'),
+    ('滑鼠');
+
+-- 插入投票紀錄
+INSERT INTO vote (voter, voting_item_id) VALUES
+    ('Leo', 1),
+    ('Sandy', 1),
+    ('Randy', 2),
+    ('RSY', 2),
+    ('tsong', 2),
+    ('tsong', 1);
+
--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,13 +1,13 @@
 
 -- 建立投票項目表
-CREATE TABLE voting_items (
+CREATE TABLE voting_item (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL
 );
 
 -- 建立投票表
-CREATE TABLE votes (
+CREATE TABLE vote (
    voter VARCHAR(255) NOT NULL,
    voting_item_id INT NOT NULL,
-   FOREIGN KEY (voting_item_id) REFERENCES voting_items(id)
+   FOREIGN KEY (voting_item_id) REFERENCES voting_item(id)
 );
